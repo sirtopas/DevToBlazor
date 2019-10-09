d8af8f017efa690d7ff509dd4409a4aed806a209
James <sirtopas@gmail.com>
2019-10-09 23:23:02 +0100

    Added page to call dev.to and retrieve new articles
    Signed-off-by: James <sirtopas@gmail.com>

---
+++ Model/Article.cs
@@ -0,0 +1,10 @@
+public class Article
+{
+    public string title { get; set; }
+
+    public string description { get; set; }
+
+    public string url { get; set; }
+
+    public User user { get; set; }
+}
